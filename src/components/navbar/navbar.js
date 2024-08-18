50a111eb5c8af8fd31479803b2546d7634264234
Sejall-pawar <sejalpawar463@gmail.com>
2024-08-18 20:49:07 +0530

    created a navbar and added styling for that

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import "./navbar.css"
+import "./../../index.css"
 
 function navbar() {
   return (
-    <div>
-        
+    <div className='header'>
+      <h1 className='header-name'>RecipeBook</h1>
+
+      <div className='header-links'>
+        <a href="/" className='nav-name'>Home</a>
+        <a href="/about" className='nav-name'>About</a>
+        <a href="/recipelist" className='nav-name'>Recipelist</a>
+      </div>
     </div>
   )
 }
